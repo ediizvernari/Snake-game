24590d59c2c8612bfa8f94b83ace99eb14a042c2
ediizvernari <edi.izvernari@student.upt.ro>
2023-06-24 22:50:03 +0300

    commit

---
+++ GameFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+
+    GameFrame(){
+
+        this.add(new GamePanel());
+        this.setTitle("Snake Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 600);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
